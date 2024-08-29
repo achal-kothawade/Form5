96f59e4ac5c5daf6d2039f9ad3b1a319b32ad2b3
achal-kothawade <68747889+achal-kothawade@users.noreply.github.com>
2024-08-29 19:37:50 +0530

    Add files via upload

--- Error.cshtml.cs
+++ Error.cshtml.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics;
 
-namespace Form5.Pages
+namespace LoginAndSession.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
